cf79a019fd7c52d5405e9099035f286ef97052d2
Zach Breman <zbreman@jellyvision.com>
2016-09-12 10:19:34 -0500

    Adding in a way to install homebrew for linux

--- homebrew/install_brew.sh
+++ homebrew/install_brew.sh
@@ -4,7 +4,14 @@ sudo -v
 if test ! $(which brew)
 then
   echo "Installing Brew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+    PATH="$HOME/.linuxbrew/bin:$PAT
+  fi
 fi
 
 brew tap homebrew/versions
